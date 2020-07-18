cd42fa2b893858e8cb0cc59ccde7280794bb356f
Fredy Somy <65971072+fredysomy@users.noreply.github.com>
2020-07-19 02:00:36 +0530

    Update apod.js

--- main/script/apod.js
+++ main/script/apod.js
@@ -1,5 +1,5 @@
 function getq() {
-    document.getElementById("pa").innerHTML=" ";
+    
     var queri=document.getElementById("query").value;
     var url="https://api.nasa.gov/planetary/apod?api_key=4iWhuYckFgwjufu2vCPzb3OB5B5gdqUeXw2PJvF3&date=";
     var fu=url+queri;
@@ -8,10 +8,14 @@ function getq() {
     .then(data => {
         dat=data['explanation'];
      dat1=data['url'];
+     var xyz=document.createElement('img');
+xyz.style.cssText="width:80%;height:80%;margin:10%;margin-top:3%;";
         console.log(dat1);
         console.log(dat);
-        document.getElementById('im').setAttribute("src",dat1);
+       xyz.setAttribute("src",dat1);
+       document.getElementById("im").appendChild(xyz);
         var exp=document.createElement("p");
+        pa.style.cssText="text-align:center;";
        var xc=document.createTextNode(dat);
        document.getElementById("pa").appendChild(xc);
 
@@ -19,6 +23,11 @@ function getq() {
 
     });
 
+
+
+
+
+}
 
 
 
