03f80d6bcde6eb15cad53482537e51707eb2f20b
fredysomy <fredysomy@gmail.com>
2020-07-20 08:45:58 +0530

    --

--- main/script/apod.js
+++ main/script/apod.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function getq() {
     document.getElementById("pa").innerHTML=" ";
     var queri=document.getElementById("query").value;
@@ -28,34 +27,3 @@ xyz.style.cssText="width:80%;height:80%;margin:10%;margin-top:3%;";
 
 
 }
-=======
-function getq() {
-    document.getElementById("pa").innerHTML=" ";
-    var queri=document.getElementById("query").value;
-    var url="https://api.nasa.gov/planetary/apod?api_key=4iWhuYckFgwjufu2vCPzb3OB5B5gdqUeXw2PJvF3&date=";
-    var fu=url+queri;
-    fetch(fu).
-    then(resp => resp.json())
-    .then(data => {
-        dat=data['explanation'];
-     dat1=data['url'];
-     xyz=document.getElementById('im');
-xyz.style.cssText="width:80%;height:80%;margin:10%;margin-top:3%;";
-        
-       xyz.setAttribute("src",dat1);
-      
-        var exp=document.createElement("p");
-        pa.style.cssText="text-align:center;";
-       var xc=document.createTextNode(dat);
-       document.getElementById("pa").appendChild(xc);
-
-
-
-    });
-
-
-
-
-
-}
->>>>>>> 0a95e08b383e7bca6f75a1956ec5571c24f3d631
--- main/script/iss.js
+++ main/script/iss.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mymap=L.map('mapid').setView([0, 0], 1);
         
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -34,40 +33,3 @@ var mymap=L.map('mapid').setView([0, 0], 1);
        
       
     });},10000);
-=======
-var mymap=L.map('mapid').setView([0, 0], 1);
-        
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoiZnJlZHlzb215IiwiYSI6ImNrY2pmZnpjcTFqanIzM284YTBya3RrMTQifQ.xhRrn00Rp1XRp4nv4IlUQQ'
-    }).addTo(mymap);
-    var icon1= L.icon({
-    iconUrl: 'img/iss.png',
-    iconSize: [50, 50],
-    iconAnchor: [25, 25],
-
-        });
-        var mark=L.marker([0,0] ,{icon:icon1}).addTo(mymap);
-    setInterval(function add() {document.getElementById("txt").innerHTML="";
-    fetch('https://api.wheretheiss.at/v1/satellites/25544').
-    then(resp => resp.json()).then(data => {
-        var lat=data.latitude;
-       
-        var lon=data.longitude;
-        mark.setLatLng([lat,lon]);
-        var tx=document.createElement("p");
-
-        var n=document.createTextNode("Latitude="+lat);
-        var n1=document.createTextNode("Longitude="+lon);
-        var di=document.getElementById("txt");
-        di.appendChild(n); 
-        di.appendChild(document.createElement("br"));
-        di.appendChild(n1);
-       
-      
-    });},5000);
->>>>>>> 0a95e08b383e7bca6f75a1956ec5571c24f3d631
